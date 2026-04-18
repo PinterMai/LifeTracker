51d8aeed219f32ee3177239ff349c55ec03f4509
PinterMai <mai.pinter1@gmail.com>
2026-04-18 18:44:24 +0200

    Move MainWindow to Views folder for proper MVVM structure
    - MainWindow.xaml in .xaml.cs premaknjeno v Views/
    - Namespace posodobljen na LifeTracker.WPF.Views
    - App.xaml StartupUri posodobljen

--- src/LifeTracker.WPF/Views/MainWindow.xaml.cs
+++ src/LifeTracker.WPF/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LifeTracker.WPF;
+namespace LifeTracker.WPF.Views;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
